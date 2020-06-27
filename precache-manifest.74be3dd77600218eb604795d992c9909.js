bc71e84a7f8e08f9be06bf9fe2a281bad03692cf
FanniTakax <tkcsfni@gmail.com>
2020-06-27 14:43:44 +0200

    Updates

---
+++ precache-manifest.74be3dd77600218eb604795d992c9909.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6907fb8a503ebf49548fb84c3beb343f",
+    "url": "/my_experimental_page/index.html"
+  },
+  {
+    "revision": "9a77a6e840953e1175d5",
+    "url": "/my_experimental_page/static/css/main.ff9b7788.chunk.css"
+  },
+  {
+    "revision": "f76f13f3cb2b47959b12",
+    "url": "/my_experimental_page/static/js/2.f6741dab.chunk.js"
+  },
+  {
+    "revision": "e9d36b14d534a681091198ba79608337",
+    "url": "/my_experimental_page/static/js/2.f6741dab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a77a6e840953e1175d5",
+    "url": "/my_experimental_page/static/js/main.70d29ad5.chunk.js"
+  },
+  {
+    "revision": "573ba0df3ec18251d10d",
+    "url": "/my_experimental_page/static/js/runtime-main.d1f48c7d.js"
+  },
+  {
+    "revision": "cbcbe7cca26bed4b7a669d9ac1554f2c",
+    "url": "/my_experimental_page/static/media/leaf.cbcbe7cc.jpg"
+  },
+  {
+    "revision": "b35e2ec3b3cba5d619d64748d03b30ba",
+    "url": "/my_experimental_page/static/media/mimi_and_fanni.b35e2ec3.jpg"
+  },
+  {
+    "revision": "86ff7c17f7e92af5da77274697bbff25",
+    "url": "/my_experimental_page/static/media/moviedb-logo.86ff7c17.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/my_experimental_page/precache-manifest.84ec8bbf8ea1059535d7d59975fca26b.js"
+  "/my_experimental_page/precache-manifest.74be3dd77600218eb604795d992c9909.js"
 );
 
 self.addEventListener('message', (event) => {
